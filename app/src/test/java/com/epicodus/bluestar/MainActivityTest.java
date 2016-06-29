3f83ebebf0702dc9f31c9634921b4575a0551ab3
Adam Craig and Nick Lyman <nick@>
2016-06-29 10:05:47 -0700

    Pass expected and actual intent test for button click from viewDonutsButton click.

--- app/src/test/java/com/epicodus/bluestar/MainActivityTest.java
+++ app/src/test/java/com/epicodus/bluestar/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.epicodus.bluestar;
 
+import android.content.Intent;
 import android.os.Build;
 import android.widget.TextView;
 
@@ -9,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 import static junit.framework.Assert.assertTrue;
 
@@ -27,5 +29,14 @@ public class MainActivityTest {
     public void validateTextViewContent() {
         TextView introTextView = (TextView) activity.findViewById(R.id.introTextView);
         assertTrue("Our donuts are made from a classic brioche recipe that originated in the south of France. The dough takes 18 hours to make and is made from scratch every day; we start with a sustainable bread flour from Shepherds Grain, add cage free eggs, whole hormone-free milk, and then fold in a European-style butter. Our donuts, glazes, and fillings are made fresh throughout the day, and we only cook our donuts in rice oil. Selections change daily and sell out quickly.".equals(introTextView.getText().toString()));
+    }
+
+    @Test
+    public void donutListActivityStarted() {
+        activity.findViewById(R.id.viewDonutsButton).performClick();
+        Intent expectedIntent = new Intent(activity, DonutListActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 }
