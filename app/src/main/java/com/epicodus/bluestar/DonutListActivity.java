519750f013b23f515dd2028c244e176bc6985a2b
Adam Craig and Nick Lyman <adam@>
2016-06-29 09:39:04 -0700

    Add ListView to display list of donuts on DonutListActivity, use ButterKnife to bind ListView

--- app/src/main/java/com/epicodus/bluestar/DonutListActivity.java
+++ app/src/main/java/com/epicodus/bluestar/DonutListActivity.java
@@ -2,12 +2,35 @@ package com.epicodus.bluestar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class DonutListActivity extends AppCompatActivity {
+
+    @Bind(R.id.donutListView) ListView mDonutListView;
+
+    private String[] donuts = new String[] {
+            "The OG",
+            "Mexican Hot Chocolate",
+            "Maple Bacon",
+            "Rose Hibiscus",
+            "Chocolate Salted Almond",
+            "The Old Fashioned",
+            "Vanilla Cake",
+            "Blue Bourbon & Basil",
+            "Lemon Poppyseed",
+            "Banana Walnut Fritter"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donut_list);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, donuts);
+        mDonutListView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/epicodus/bluestar/MainActivity.java
+++ app/src/main/java/com/epicodus/bluestar/MainActivity.java
@@ -1,13 +1,29 @@
 package com.epicodus.bluestar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ViewPropertyAnimatorCompatSet;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.viewDonutsButton) Button mViewDonutsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mViewDonutsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DonutListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
